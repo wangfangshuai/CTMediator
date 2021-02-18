edd65889ab5a132be1ec07ced07b320b8022591f
王方帅 <wangfangshuai@apusapps.com>
2021-02-18 16:26:28 +0800

    修改CTMediator支持Package.swift
    修改CTMediator支持Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+  name: "CTMediator",
+  platforms: [
+    .iOS(.v8), .tvOS(.v9),
+  ],
+  products: [
+    .library(name: "CTMediator", targets: ["CTMediator"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(name: "CTMediator"),
+  ]
+)
